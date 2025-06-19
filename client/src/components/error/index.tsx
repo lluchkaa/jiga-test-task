e50eae1a57a71a65f66761281d423d8400f43233
Yura Luchka <lluchkaa@gmail.com>
2025-06-19 17:03:43 +0300

    add error compnent

---
+++ client/src/components/error/index.tsx
@@ -0,0 +1,16 @@
+import { cn } from "~/services/styles"
+
+type Props = {
+  message?: string
+  className?: string
+}
+
+export const ErrorAlert: React.FC<Props> = (props) => {
+  const { className, message = "An error occurred" } = props
+
+  return (
+    <div className={cn("flex items-center justify-center", className)}>
+      <p className="text-red-300">{message}</p>
+    </div>
+  )
+}
