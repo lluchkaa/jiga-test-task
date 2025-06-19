64fd886e377e1bfb4be4aba077c302aedb7ca592
Yura Luchka <lluchkaa@gmail.com>
2025-06-19 10:44:51 +0300

    add application bootstrap

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,16 @@
 import { env } from "~/lib/env"
+import { bootstrap } from "~/lib/bootstrap"
 
-console.log(`Hello, World from port ${env.PORT}!`)
+function main() {
+  const app = bootstrap()
+
+  app.listen(env.PORT, (error) => {
+    if (error) {
+      console.error("Error starting the server:", error)
+      process.exit(1)
+    }
+    console.log(`Server is running on port ${env.PORT}`)
+  })
+}
+
+main()
---
+++ server/src/lib/bootstrap/index.ts
@@ -0,0 +1,7 @@
+import express from "express"
+
+export function bootstrap() {
+  const app = express()
+
+  return app
+}
