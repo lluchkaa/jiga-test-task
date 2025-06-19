fba5ae5b56902cb9906d66bbe8064b4f42a6aff6
Yura Luchka <lluchkaa@gmail.com>
2025-06-19 18:52:48 +0300

    fix error handler params list

--- server/src/lib/error/index.ts
+++ server/src/lib/error/index.ts
@@ -1,6 +1,6 @@
 import { ErrorRequestHandler } from "express"
 
-export const errorHandler: ErrorRequestHandler = (err, _req, res) => {
+export const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
   console.error(err)
   res.status(500).json({
     message: "An unexpected error occurred",
