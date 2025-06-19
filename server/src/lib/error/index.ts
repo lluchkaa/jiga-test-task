4c1c2bb8764be3f38e3ab1f997288a0768b987d3
Yura Luchka <lluchkaa@gmail.com>
2025-06-19 18:55:44 +0300

    fix ts-eslint error

--- server/src/lib/error/index.ts
+++ server/src/lib/error/index.ts
@@ -1,5 +1,6 @@
 import { ErrorRequestHandler } from "express"
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
   console.error(err)
   res.status(500).json({
