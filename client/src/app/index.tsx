f61b4528e034d38eada693e698994b7ea0d46e94
Yura Luchka <lluchkaa@gmail.com>
2025-06-19 15:58:40 +0300

    add tailwindcss

--- client/src/app/index.tsx
+++ client/src/app/index.tsx
@@ -1,4 +1,6 @@
-import { QuoteComparisonPage } from "./quote-comparison/page"
+import { QuoteComparisonPage } from "~/app/quote-comparison/page"
+
+import "~/assets/styles/style.css"
 
 export const App: React.FC = () => {
   return <QuoteComparisonPage />
